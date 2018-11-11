635ddeb4882b745828bc3907efea19c4fd77be4b
Truong <Phuc.Truong@bluescope.com>
2018-11-12 00:48:40 +0700

    Edit welcome pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Phuc Truong. Hihi
                 </div>
 
                 <div class="links">
