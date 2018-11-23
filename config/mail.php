5b94dba3f928cd374212c0f9aecd97034312cee6
Truong <Phuc.Truong@bluescope.com>
2018-11-23 16:01:25 +0700

    fix file .env

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'noreply@phuctruong.net'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@phuctruong.net'),
         'name' => env('MAIL_FROM_NAME', 'Phuc Truong'),
     ],
 
